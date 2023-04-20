0dfbdc1d195b6d195d0e3a9028c077d5d29ae9f3
Oman Kumar <100030700+Okiitr@users.noreply.github.com>
2023-04-20 22:35:29 +0530

    count factorial palindrome total zeros methods

--- src/BASIC.java
+++ src/BASIC.java
@@ -1,30 +1,80 @@
 package com.company;
 
-class Employee{
-    int id;
-    String name;
-    private int salary;
-    public void setSalary(int salary){this.salary=salary;}
-    public int getSalary(){
-        return salary;
-    }
-    public Employee(int i,String n){
-        id =i;
-        name = n;
-    }
-    public void details(){
-        System.out.println("your id is : "+ id);
-        System.out.println("your name is " + name);
-        System.out.println("your salary is "+salary);
-    }
 
-}
+import java.math.BigInteger;
+import java.util.Scanner;
+
 public class BASIC {
+    static int CountDigit(int x){
+        int num = x;
+        int count = 0;
+        while(num!=0){
+             num = num/10;
+             count++;
+        }
+        return count;
+    };
+    static boolean IsPalindrome(int n) {
+        int num =n;
+        int res=0;;
+        while ( num!=0){
+            res = res*10+num%10;
+            num/=10;
+        }
+        num =n;
+        if(num==res) {
+            System.out.println("your number "+num+" is a palindrome");
+            return true;
+        }else {System.out.println("your number "+num+" NOT a palindrome");
+            return false;}
+
+    };
+    static int Factorial1(int n){
+        if(n==0 || n==1){
+            return 1;
+        }
+        else {
+            return n*Factorial1(n-1);
+        }
+    };
+    static int Factorial2(int x){
+        int fact=1 ;
+//
+        for(int i = 1; i <= x; ++i) {
+            fact *= i;
+        }
+        return fact;
+    };
+    public static BigInteger factorial(int n) {
+        BigInteger result = BigInteger.ONE;
+        for (int i = 1; i <= n; i++) {
+            result = result.multiply(BigInteger.valueOf(i));
+        }
+        return result;
+    };
+    public static int TotalZero(int n){
+        int zeros = 0;
+        int m = 5;
+        int x = n;
+        while (x>0){
+            zeros = zeros+n/m;
+            x=x/m;
+            m = m*5;
+        }
+        return zeros;
+    };
     public static void main(String[] args) {
-        Employee oman = new Employee(411, "Oman kumar");
-        oman.setSalary(750000);
-        System.out.println(oman.getSalary());
-        oman.details();
+        Scanner num = new Scanner(System.in);
+        System.out.println(" Enter a number for count digit");
+        int x = num.nextInt();
+        System.out.println(" Enter a number for Factorial");
+        int n = num.nextInt();
+        System.out.println("total numbers in "+ x + " is = "+ CountDigit(x));
+        System.out.println(IsPalindrome(x));
+//        System.out.println("M1-factorial of below 25 "+n+" is "+Factorial1(n));
+        System.out.println("total no of zeros in factorial "+ TotalZero(n));
+        System.out.println("factorial of above 25 "+n+" is "+factorial(n));
+
     }
 }
 
--- src/Factorial.java
+++ src/Factorial.java
@@ -4,24 +4,33 @@ package DSA.math;
 import java.util.Scanner;
 
 public class Factorial{
-    static int fact1(int n){
+    static double fact1(int n){
         if(n==1 || n==0){
             return 1;
         }
         return n*fact1(n-1);
     }
-    static int fact2(int n){
+    static double fact2(int n){
         int fact =1;
         for(int i=1; i<=n; i++){
             fact*=i;
         }
         return fact;
     }
+    public static int TotalZero(int n){
+        int zeros =0;
+        int m =5;
+        while (n!=0){
+            zeros =zeros+n/m;
+            m*=5;
+        }
+        return zeros;
+    };
 //both the function having theta n time complexity but recursive function has theta n space complexity and other has theta 1
     public static void main(String[] args) {
         System.out.println("enter a number to find factorial");
         Scanner scan =new Scanner(System.in);
         int n = scan.nextInt();
-        System.out.format("factorial of %d is %d and %d ",n,fact1(n),fact2(n) );
+        System.out.println("factorial "+fact1(n)+" and "+ fact2(n)+ " total zeros "+ TotalZero(n));
     }
 }
