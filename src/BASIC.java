e7ac9181fc035197b63cb17ffa8340296ac336ab
Oman Kumar <100030700+Okiitr@users.noreply.github.com>
2023-04-21 22:33:41 +0530

    count factorial palindrome total zeros methods

--- src/BASIC.java
+++ src/BASIC.java
@@ -63,17 +63,61 @@ public class BASIC {
         }
         return zeros;
     };
+    public static boolean IsPrime(int n){
+        int value =  (int)(Math.sqrt(n))+1;
+        boolean isprime = true ;
+        for(int i=2; i<value; i++){
+            if (n==2 || n==3){
+                isprime= true;
+            } else if (n>=4) {
+                if (n%i==0){
+                    isprime =false;
+                    break;
+                }else{
+                    isprime= true;
+                }
+            }
+        }
+        return isprime;
+    };
+//    public static int HCF(int x ,int y){
+//        int fact = 1;
+//        int min = Math.min(x,y);
+//        for(int i=1; i<=min; i++){
+//            if(x%i==0 && y%i==0 && IsPrime(i)){
+//                fact*=i;
+//            }
+//        }
+//        return fact;
+//    };
+//    public static int HCF(int x ,int y){
+//        int fact = 1;
+//        int min = Math.max(x,y);
+//        for(int i=1; i<=min; i++){
+//            if(x%i==0 && y%i==0){
+//                fact*=i;
+//            }
+//        }
+//        return fact;
+//    };
     public static void main(String[] args) {
         Scanner num = new Scanner(System.in);
-        System.out.println(" Enter a number for count digit");
+        System.out.println("Enter a number for count digit");
         int x = num.nextInt();
-        System.out.println(" Enter a number for Factorial");
+        System.out.println("Enter a number for Factorial");
         int n = num.nextInt();
-        System.out.println("total numbers in "+ x + " is = "+ CountDigit(x));
+        System.out.println("Total numbers in "+ x + " is = "+ CountDigit(x));
         System.out.println(IsPalindrome(x));
-//        System.out.println("M1-factorial of below 25 "+n+" is "+Factorial1(n));
+//        System.out.println("M1-factorial of below 25 "+n+" is "+Factorial1(n));51545
         System.out.println("total no of zeros in factorial "+ TotalZero(n));
         System.out.println("factorial of above 25 "+n+" is "+factorial(n));
+        System.out.println("enter a num for prime ");
+        int p = num.nextInt();
+        System.out.println("your given number is prime "+IsPrime(p));
+//        System.out.println("enter two number for their hcf and lcf");
+//        int a = num.nextInt();
+//        int b = num.nextInt();
+//        System.out.println("HCF of the numbers "+a+" and "+b+" "+HCF(a,b));
 
     }
 }
