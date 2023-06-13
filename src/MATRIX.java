672160e78e8086aed5d29dbba7317932b3a3f719
Oman Kumar <100030700+Okiitr@users.noreply.github.com>
2023-06-13 22:50:43 +0530

    sorting almost complete

--- src/MATRIX.java
+++ src/MATRIX.java
@@ -3,12 +3,23 @@ package DSA.Matrix;
 class MATRIX{
     public static void main(String[] args) {
         int [] [] matrix= {{1,2,3},{4,5,6},{7,8,9}};
-//        snakePattern(matrix);
-//        transposeOfMatrix(matrix);
+       /* snakePattern(matrix);
+        transposeOfMatrix(matrix);
         printMatrix(matrix);
-//        boundaryTraversal(matrix);
+        boundaryTraversal(matrix);
         rotate90AntiClock(matrix);
-        printMatrix(matrix);
+        printMatrix(matrix);*/
+
+        int [][] arr = {{1,1},{2,2}};
+        int [][] brr = {{1,2,3},{1,1,1}};
+        printMatrix(multiplicationOfMatrix(arr,brr));
+
+        int a[][] = {{1, 2, 3, 4},
+            {5, 6, 7, 8},
+            {6, 10, 11, 12},
+            {13, 14, 15, 16}};
+        spiralTraversal(a);
+        System.out.println(findElementInRow_ColSortedMatrix(a,9));
     }
 
     public static void printMatrix(int [] [] arr){
@@ -95,5 +106,75 @@ class MATRIX{
             for(int i =row-2; i>0; i--)
                 System.out.print(arr[i][0]+" ");
         }
+    }
+    public static void spiralTraversal(int[][]arr){
+        int row = arr.length , col = arr[0].length;
+        int top = 0, right = col-1, bottom = row-1 , left =0;
+
+        // Time Complexity: O(m*n).
+
+        while (top<bottom && left<right){
+
+            for(int i =left; i<=right; i++){   // printing top row
+                System.out.print(arr[top][i]+" ");}
+            top++;
+
+            for(int i =top; i<=bottom; i++){     // printing left column
+                System.out.print(arr[i][right]+" ");}
+            right--;
+
+            if(top<=bottom) {
+            for(int i =right; i>=left; i--)
+                System.out.print(arr[bottom][i]+" ");
+            bottom--;
+            }
+
+            if(left<=right) {
+                for (int i = bottom; i >= top; i--)
+                    System.out.print(arr[i][left] + " ");
+                left++;
+            }
+        }
+    }
+
+    public static int [][] multiplicationOfMatrix (int [] []arr, int [][] brr){ //O(r1*c2*c1)
+        int r1 = arr.length, c1 = arr[0].length , r2 = brr.length , c2 = brr[0].length;
+        int [][] result = new int[r1][c2];
+        if(c1==r2){
+            for(int i  = 0;  i < r1; i++){
+
+                for( int j = 0;  j < c2; j++){
+
+                    for ( int k =0; k<c1; k++){
+                        result[i][j] += arr[i][k]*brr[k][j];
+                    }
+                }
+            }
+        }
+        else{
+            System.out.println("multiplication of matrixes are not possible");
+        }
+        return result;
+    }
+
+    public static boolean findElementInRow_ColSortedMatrix (int [] [] arr, int x){
+
+        int row = arr.length,  col= arr[0].length;
+        int top =0 , right =col-1;
+
+        while (top<row && right>=0){
+            if(arr[top][right]==x){
+                return true;
+            }
+            else if(arr[top][right]<x){
+                top++;
+            }
+            else {
+                right--;
+            }
+
+        }
+
+        return false;
     }
 }
--- src/SORTING.java
+++ src/SORTING.java
@@ -5,7 +5,7 @@ import java.util.*;
 class SORTING {
     public static void main(String[] args) {
        /* int [] arr = {3,2,33,4,5,44,5,666,5,4,33,7,8};
-        inBuiltSortingFunctions(arr);*/
+        inBuiltSortingFunctions(arr);
 
         // bubble sort
         int [] bubble_arr = { 23,4,55,30,44,3,55,33,87,90};
@@ -22,13 +22,54 @@ class SORTING {
         //merge two sorted array
         int [] arr ={2,4,9,33,44,90};
         int [] brr ={20,41,44,95};
-        int [] mer ={2,4,9,33,44,90, 20,41,44,95};
-        int [] mer1 ={2,4,9,33,44,90, 22,43,44,100};
+        int [] mer ={2,4,9,33,90, 20,41,44,95};
+        int [] mer1 ={2,4,9,33,44,90,43,44,100};
         mergeTwoSortedArrays(arr,brr);
         merge(mer,0,5,9);
         mergeSort(mer1,0,9);
         System.out.println(Arrays.toString(mer1));
-}
+
+        int [] inverse ={20,40,9,13,34,12};
+        System.out.println(countInversion(inverse,0,5));
+
+        int [] sorted_array1 = { 2,4,4,5,9,11,24,24,45,50};
+        int [] sorted_array2 = { 2,3,7,9,15,24,45,45 ,60};
+        intersectionOfTwoSortedArrays(sorted_array1,sorted_array2);
+        System.out.println( );
+        unionOfTwoSortedArrays(sorted_array1,sorted_array2);
+
+
+        //partition and quick sort
+
+        int [] naive ={ 2,11,45,15,32,4,15,10,6,16};
+        int n = naive.length;
+        naivePartition(naive,0,n-1,n-1);
+        System.out.println(Arrays.toString(naive));
+
+        int [] lomuto ={ 2,11,45,15,32,4,15,10,6,12};
+        int n1 = lomuto.length;
+        lomutoPartion(lomuto,0,n-1);
+        System.out.println(Arrays.toString(lomuto));
+
+        int [] hoare ={13 ,11,45,15,32,4,15,10,6,12};
+        int n2 = hoare.length;
+        hoarePartion(hoare,0,n2-1);
+        System.out.println(Arrays.toString(hoare));
+        lomutoPartion(hoare,0,n2-1); // in lomuto pivot will be always in middle but in hoare its at random position
+        System.out.println(Arrays.toString(hoare));  */
+
+        int [] quick_sortl = {34,3,43,29,89,33,4,20,56};
+        int [] quick_sorth = {11,45,34,55,8,90,56,23,17};
+        quickSortLomuto(quick_sortl,0,8);
+        quickSortHoare(quick_sorth,0,8);
+        System.out.println(Arrays.toString(quick_sortl));
+        System.out.println(Arrays.toString(quick_sorth));
+        int [] small = {23,4,55,3,44,40,6,75,40};
+        System.out.println(kthSmallestElement(small,5));
+
+
+
+    }
     public static void inBuiltSortingFunctions(int [] arr){
         // for arrays only Arrays.sort()  time complexity is O(n log(n)) same for collection.sort
 
@@ -127,6 +168,10 @@ class SORTING {
         System.out.println(Arrays.toString(arr));
     }
 
+    /* merge sort starts from here
+    * T(n) = 2T(n/2) + Θ(n)
+    * Time complexity of Merge Sort is Θ(nLogn) in all 3 cases (worst, average and best)
+    * as merge sort always divides the array in two halves and take linear time to merge two halves */
     public static void mergeTwoSortedArrays(int []arr, int []brr){
         int n = arr.length;
         int m = brr.length;
@@ -203,6 +248,293 @@ class SORTING {
             merge(arr, l, m, r);
         }
     }
+    public static int countAndMerge(int []arr, int low, int mid, int high){
+        int n1 =mid-low+1, n2 = high-mid;
+        int [] left =new int[n1];
+        int [] right= new int[n2];
+
+        for(int i=0; i<n1; i++){
+            left[i]=arr[i+low];
+        }
+        for(int i=0; i<n2; i++){
+            right[i]=arr[i+mid+1];
+        }
+        int res=0, i=0,j=0 , k = low;
+        while (i<n1 && j<n2 ){
+            if(left[i]<=right[j]){
+                arr[k]=left[i];
+                k++;
+                i++;
+            }
+            else {  // when right element is smaller then update the res
+                arr[k]=right[j];
+                res =res+(n1-i);
+                k++;;
+                j++;
+            }
+        } // time complexity O(n) ans space also O(n);
+        while (i<n1){ arr[k]=left[i]; k++; i++;}
+        while (j<n2){ arr[k]=right[j]; k++; j++; }
+        return res;
+//        System.out.println(Arrays.toString(arr));
+    }
+
+    public static int  countInversion(int arr[], int l, int r)
+    { //Θ(nLogn)
+        int res =0;
+        if (l < r) {
+            // Find the middle point
+            int m = l + (r - l) / 2;
+
+            // Sort first and second halves
+            res+= countInversion(arr, l, m);
+            res+= countInversion(arr, m + 1, r);
+
+            // Merge the sorted halves
+            res+= countAndMerge(arr, l, m, r);
+        }
+        return res;
+    }
+    public static void intersectionOfTwoSortedArrays (int [] arr, int []brr){
+        int n = arr.length, m =brr.length;
+
+        int i =0, j=0;
+        while ( i<n && j<m ){
+            if(i>0 && arr[i]==arr[i-1]){  // to avoid duplicates  O(N + M)  O(max(m*log(m), n*log(n)) + min(m, n) )
+                i++;
+                continue;
+            }
+            if(arr[i]==brr[j])
+            {
+                System.out.print(arr[i]+" ");
+                i++;
+                j++;
+            }
+            else if ( arr[i]<brr[j]){
+                i++;
+            }
+            else{
+                j++;
+            }
+        }
+
+    }
+
+    public static void unionOfTwoSortedArrays (int [] arr, int []brr){
+        int n = arr.length, m =brr.length;
+        // O(n+m).
+        int i =0, j=0;
+        while ( i<n && j<m ){
+            if(i>0 && arr[i]==arr[i-1]){
+                i++;
+                continue;
+            }
+            if(j>0 && brr[j]==brr[j-1]){  // we have to check duplicates for both the arrays
+                j++;
+                continue;
+            }
+            if(arr[i]==brr[j])
+            {
+                System.out.print(arr[i]+" ");
+                i++;
+                j++;
+            }
+            else if ( brr[j]<arr[i]){
+                System.out.print(brr[j]+" ");
+                j++;
+            }
+            else{
+                System.out.print(arr[i]+" ");
+                i++;
+            }
+        }
+        while ( i<n) {   // to print rest elements
+            if( arr[i]==arr[i-1]){
+                i++;
+                }
+            else {
+                System.out.print(arr[i]+" ");
+                i++;
+            }
+        }
+
+        while ( j<m) {
+            if( brr[j]==brr[j-1]){
+                j++;
+            }
+            else {
+                System.out.print(brr[j]+" ");
+                j++;
+            }
+        }
+
+    }
+
+
+    /*Naive partition: In this partition helps to maintain the relative order of the elements but this partition takes O(n) extra space.
+    Lomuto partition: In this partition, The last element chooses as a pivot in this partition.
+    The pivot acquires its required position after partition but more comparison takes place in this partition.
+    Hoare's partition: In this partition, The first element chooses as a pivot in this partition.
+    The pivot displaces its required position after partition but less comparison takes place as compared to the Lomuto partition.*/
+
+
+    public static void naivePartition ( int[]arr,int l, int h, int p){
+
+        int n = arr.length;
+        int [] temp = new int [h-l+1];
+        int index = 0;
+
+        for(int i =l; i<=h; i++){
+            if(arr[i]<=arr[p] && i!=p){
+                temp[ index++] = arr[i];
+
+            }
+        }
+
+        temp[index]=arr[p];  // temp[index++]=arr[p];
+        index++;
+        // here we are assigning pivot on index+1 position and increasing value of index also same
+        for(int i =l; i<=h; i++){
+            if(arr[i]>arr[p]){
+                temp[index] = arr[i];
+                index++;
+            }
+        }
+
+        for (int i = l; i<=h; i++){
+            arr[i]=temp[i-l];
+        }
+
+    }
+
+    public static int lomutoPartion (int []arr,int l , int h){
+        // consider that the pivot is arr[h]
+           // for the best case use random pivot
+       /* int p = (int)(Math.random() * (h-l+1)) + l;   this case will always take time nlogn in every case;
+        int tmp = arr[h];
+        arr[h] =arr[p];
+        arr[p] = tmp;
+        int pivot = arr[p]; */
+
+        int pivot = arr[h];
+        int i =l-1; // this is out  <pivot band
+        for(int j = l; j<=h; j++){
+            if(arr[j]<pivot){
+                int tmp = arr[i+1];
+                arr[i+1] =arr[j];
+                arr[j] = tmp;
+                i++;
+            }
+        }
+        int position = i+1;
+        int tmp = arr[i+1];
+        arr[i+1] = pivot;
+        arr[h] = tmp;
+        return position;
+    }
+
+    static int kthSmallestElement(int arr[],int k){
+        int n = arr.length;
+        int l=0,r=n-1;
+        while(l<=r){
+            int p=lomutoPartion(arr,l,r);
+            if(p==k-1)
+                return arr[p];
+            else if(p>k-1)
+                r=p-1;
+            else
+                l=p+1;
+        }
+        return -1;
+    }
+
+    public static int hoarePartion (int []arr,int l , int h){
+        // consider that the pivot is arr[h]
+        int pivot = arr[l];
+        int i =l-1;     // this is out  <= pivot band
+        int j = h+1;   // this is out  >pivot band
+        while (true){
+            do{
+                i++;
+            }while(arr[i]<pivot);
+            do {
+                j--;
+            } while (arr[j]>pivot);
+            if(i>=j){
+                return j;
+            }
+            int tmp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = tmp;
+        }
+    }
+  /*Best Case: The best case occurs when the partition process always picks the middle element as pivot.
+     Following is recurrence for best case.   T(n) = 2T(n/2) + Θ(n)   Θ(nLogn).
+  */
+    /* the worst case would occur when the array is already sorted in increasing or decreasing order.
+    Following is recurrence for worst case.
+     T(n) = T(n-1) + Θ(n)    Θ(n2).
+     here 0(N) time is required for partions
+
+     it requires 0(logn ) space for recursion call its inplace algo that don't use any extra array to copy element
+     but extra space it will take
+     */
+    public static void  quickSortLomuto (int [] arr,int l, int h){
+        if(l<h){
+            int p = lomutoPartion(arr,l,h); // it will return to position of pivot
+            quickSortLomuto(arr,l,p-1);  // we sort before pivot
+            quickSortLomuto(arr,p+1,h);  // then we sort after pivot
+
+        }
+    }
+
+    /* for tail elimination in quict sort*/
+    public static void  quickSortLomutoTailElimination (int [] arr,int l, int h){
+        if(l<h){
+            int p = lomutoPartion(arr,l,h); // it will return to position of pivot
+            if (p - l < h - p) {
+                quickSortLomutoTailElimination(arr, l, (p-1));
+                l= p+1;
+            } else {
+                quickSortLomutoTailElimination(arr, p+1, h);
+                h = p - 1;
+            }
+
+        }
+    }
+    public static void  quickSortHoare (int [] arr,int l, int h){
+        if(l<h){
+            int p = hoarePartion(arr,l,h);  //this will give the index upto all are less than pivot
+            quickSortHoare(arr,l,p);  // sorting less then pivot values
+            quickSortHoare(arr,p+1,h);  // sorting equal or greater than pivot values
+
+        }
+    }
+
+    static void cycleSortDistinct(int []arr, int n)
+    {
+        for(int cs=0;cs<n-1;cs++){
+            int item=arr[cs];
+            int pos=cs;
+            for(int i=cs+1;i<n;i++)
+                if(arr[i]<item)
+                    pos++;
+            //swap(item,arr[pos])
+
+            int temp=item;
+            item=arr[pos];
+            arr[pos]=temp;
+            while(pos!=cs){
+                pos=cs;
+                for(int i=cs+1;i<n;i++)
+                    if(arr[i]<item)
+                        pos++;
+
+                //swap(item,arr[pos])
+                temp=item;
+                item=arr[pos];
+                arr[pos]=temp;
+            }
 
 }
 
